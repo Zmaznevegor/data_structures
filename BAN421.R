2c11195a3b962cd187d256119ee68288121f8fd3
Zmaznevegor <zmaznevegor@gmail.com>
2019-11-21 18:57:57 +0100

    Key updates: 1. Preprocessed and summarised the data for production-consumption analysis
    2. Added part with the HMM model for the Norway production state based on the Nordic countries production
    
    3. Visualisation for the HMM model and actual state
    
    4. Minor commenting changes
    
    Up next: creating a for-loop to define the biggest dependence of NOrway out of all other nordic countries

--- BAN421.R
+++ BAN421.R
@@ -1,23 +1,19 @@
-# In this paper, we try to investigate the influence of other countries' supply(production)
-#and consumption on the electricity market spot prices in Norway including Bergen and Tromsø
-#we first will explore the more efficient way to load data
-#then explore different statistical methods and R packages
-#finally we will visualize our output
-
-library("dplyr")
-library("ggplot2")
-library("magrittr")
+# Load libraries----
+library(dplyr)
+library(ggplot2)
+library(magrittr)
 library(readxl)
 library(tibble)
 library(readxl)
+library(depmixS4)
+library(quantmod)
+library(MASS)
+library(reshape2)
+library(gridExtra)
 
+## 1.Data cleaning process ----
 
-#SETTING WORK DIRECTORY
-setwd("/Users/Yako/Desktop/BAN421/Data")
-
-#____________________1.Data cleaning process _______________________
-
-#find the file names of consumption, spot prices and production for each year
+# find the file names of consumption, spot prices and production for each year
 consum_names <- list.files(path = "/Users/Yako/Desktop/BAN421/Data", 
                            pattern = "consumption-per.*.xlsx", full.names = T)
 elspot_names<-list.files(path = "/Users/Yako/Desktop/BAN421/Data",
@@ -34,19 +30,19 @@ system.time(for(i in consum_names){
    na.omit()
 })
 
-#we can also try to use tibble and compare
+# we can also try to use tibble and compare
 consumption_tib<-tibble()
 system.time(for(i in 1:length(consum_names)){
   consumption_tib <- read_xlsx(consum_names[i])%>% 
     bind_rows(consumption_tib)  
 })
-#only takes the columns that we need
-#the processing time is almost same 
-#check the data structure in each, and found out they have same data structure
+# only takes the columns that we need
+# the processing time is almost same 
+# check the data structure in each, and found out they have same data structure
 str(consumption) 
 str(consumption_tib)
 
-#so we continue to use dataframe to load production and price data
+# so we continue to use dataframe to load production and price data
 production <-data.frame()
 for(i in 1:length(production_names)){
   production <- read_xlsx(production_names[i])%>% 
@@ -62,8 +58,8 @@ for(i in 1:length(elspot_names)){
 price<-price[,-5]
 price<-na.omit(price)
 
-#continue to load the prognosis data for consumption and production
-#so that we can use that to predict based on our model
+# continue to load the prognosis data for consumption and production
+# so that we can use that to predict based on our model
 consum_pre<-read_xlsx("consumption-prognosis_2019_hourly.xlsx")
 produc_pre<-read_xlsx("production-prognosis_2019_hourly.xlsx")
 # sum up all productions from small areas in each country and take the total only
@@ -73,14 +69,15 @@ produc_pre<- produc_pre%>%
   mutate(DK=DK1+DK2) %>%
   select("Date","Hours","FI","DK","EE",
          "LV","LT","NO","SE")
-#save all these data as R.data
+
+# save all these data as R.data
 
 save(consum_pre,consumption,price,produc_pre,production,file="BAN421.Rdata")
 
-#_____________2.ploting_________________________
-#before we explore the relationship, we can plot the trend in these 5 years first
+## 2.Plotting ----
+# before we explore the relationship, we can plot the trend in these 5 years first
 
-#create another "year" variable 
+# create another "year" variable 
 consumption<-consumption%>%
   mutate(year = format(Date, "%Y"))
 production<-production%>%
@@ -149,5 +146,80 @@ ggplot(data=plot_price,mapping=aes(x=year))+
   ggtitle("price trend")+
   ylab("mean_price")
 
-#________________3.statistical methods_______________________
+## 3.Statistical methods ----
+# Combine tables with all the data and preproceess the data
+comb <-  inner_join(consumption, production,
+                   by = c("Date", "Hours"))
 
+comb <- comb %>% 
+  mutate(NO = NO.y - NO.x,
+         DK = DK.y - DK.x,
+         SE = SE.y - SE.x,
+         NORD = Nordic.y - Nordic.x)
+
+comb <- comb[c("Date", "Hours", "NO", "DK", "SE", "NORD")]
+
+comb <- comb %>% 
+  group_by(Date) %>% 
+  summarise(NO = sum(NO),
+            DK = sum(DK),
+            SE = sum(SE),
+            NORD = sum(NORD))
+
+comb <- comb %>% 
+    mutate(NO_state = as.logical(NO > 0),
+           DK_state = as.logical(DK >0),
+           SE_state = as.logical(SE > 0),
+           NORD_state = as.logical(NORD > 0))
+
+comb <- as.data.frame(lapply(comb, function(y) gsub("TRUE", "Surplus", y)))
+comb <- as.data.frame(lapply(comb, function(y) gsub("FALSE", "Deficit", y)))
+
+comb[6:9] <- lapply(comb[6:9], as.character)
+comb$Date <- as.Date(comb$Date)
+
+# Define the price fluctuations
+fluc <- diff(log(as.numeric(comb$NORD)))
+fluc <- c(0, fluc)
+
+# Run and fit HMM
+hmm <- depmix(fluc ~ 1, 
+              family = gaussian(), 
+              nstates = 2, 
+              data = comb)
+
+fit.hmm <- fit(hmm)
+est.states <- posterior(fit.hmm)
+
+# Analyse the results
+tbl <- table(est.states$state, comb$NO_state)
+colnames(est.states)[2:3] <- c("Deficit", "Surplus")
+est.states$Date <- comb$Date
+
+grep("Deficit", comb$NO_state)
+grep("1", est.states$state)
+
+est.states$state <- gsub("1", "Deficit", est.states$state)
+est.states$state <- gsub("2", "Surplus", est.states$state)
+tbl
+
+# Plot the results
+mycols <- c("darkmagenta", "turquoise")
+
+g1 <- ggplot(comb, aes(x = Date, y = NO_state, fill = NO_state, col = NO_state)) + 
+         geom_bar(stat = "identity", alpha = I(0.75)) + 
+         scale_fill_manual(values = mycols, labels = c("Deficit", "Surplus")) +
+         scale_color_manual(values = mycols, labels = c("Deficit", "Surplus")) +
+         theme(axis.ticks = element_blank(), axis.text.y = element_blank()) +
+         labs(y = "Actual State")
+
+g2 <- ggplot(est.states, aes(x = Date, y = state, fill = state, col = state)) + 
+  geom_bar(stat = "identity", alpha = I(0.75)) +
+  scale_fill_manual(values = mycols, name = "State:\nProduction of\nNorway", labels = c("Deficit", "Surplus")) +
+  scale_color_manual(values = mycols, name = "State:\nProduction of\nNorway", labels = c("Deficit", "Surplus")) +
+  theme(axis.ticks = element_blank(), axis.text.y = element_blank()) + 
+  labs(y = "Estimated State")
+
+grid.arrange(g1, g2, 
+             widths = 1, 
+             nrow = 2)
