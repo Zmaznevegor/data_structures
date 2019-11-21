cbda4154e18a605f9b3d3edc9f1451397fa91ff3
Zmaznevegor <zmaznevegor@gmail.com>
2019-11-22 00:39:05 +0100

    Key updates: 1. Set hmm modelling as a function 2. Set plotting as a function 3. Comments to the 3rd part are added 4. Results are run for the NORD, SE and DK

--- BAN421.R
+++ BAN421.R
@@ -147,18 +147,21 @@ ggplot(data=plot_price,mapping=aes(x=year))+
   ylab("mean_price")
 
 ## 3.Statistical methods ----
-# Combine tables with all the data and preproceess the data
+# Combine tables with all the data and preproceess the data----
 comb <-  inner_join(consumption, production,
                    by = c("Date", "Hours"))
 
+# Add new parameters to the combined table based on the production and consumption parameters
 comb <- comb %>% 
   mutate(NO = NO.y - NO.x,
          DK = DK.y - DK.x,
          SE = SE.y - SE.x,
          NORD = Nordic.y - Nordic.x)
 
+# Clean the table
 comb <- comb[c("Date", "Hours", "NO", "DK", "SE", "NORD")]
 
+# Summarise by date to cancel minor chages and noise
 comb <- comb %>% 
   group_by(Date) %>% 
   summarise(NO = sum(NO),
@@ -166,6 +169,7 @@ comb <- comb %>%
             SE = sum(SE),
             NORD = sum(NORD))
 
+# Add logical parameters showing the state of each country
 comb <- comb %>% 
     mutate(NO_state = as.logical(NO > 0),
            DK_state = as.logical(DK >0),
@@ -175,51 +179,84 @@ comb <- comb %>%
 comb <- as.data.frame(lapply(comb, function(y) gsub("TRUE", "Surplus", y)))
 comb <- as.data.frame(lapply(comb, function(y) gsub("FALSE", "Deficit", y)))
 
+# Convert to the proper formatting
 comb[6:9] <- lapply(comb[6:9], as.character)
 comb$Date <- as.Date(comb$Date)
 
-# Define the price fluctuations
-fluc <- diff(log(as.numeric(comb$NORD)))
-fluc <- c(0, fluc)
+# Run and fit HMM----
+hmm <- function (param, data){
+  
+  # We will use the changes in fluctuations as dependece ground for the HMM
+  fluc <- diff(log(as.numeric(param)))
+  fluc <- c(0, fluc)
+  
+  # Set the model
+  model <- depmix(fluc ~ 1, 
+                family = gaussian(), # normal (gaussian) param distribution
+                nstates = 2, # number of states
+                data = comb) # used data 
+  
+  # Fit the data and get it as data frame with the defined states
+  fit.hmm <- fit(model)
+  est.states <- posterior(fit.hmm)
+  
+  # Analyse the results
+  tbl <- table(est.states$state, comb$NO_state)
+  colnames(est.states)[2:3] <- c("Deficit", "Surplus")
+  est.states$Date <- comb$Date
+  
+  # Change the naming of the states based on the tbl
+  grep("Deficit", comb$NO_state)
+  grep("1", est.states$state)
+  
+  est.states$state <- gsub("1", "Deficit", est.states$state)
+  est.states$state <- gsub("2", "Surplus", est.states$state)
+  
+  return(est.states)
+}
 
-# Run and fit HMM
-hmm <- depmix(fluc ~ 1, 
-              family = gaussian(), 
-              nstates = 2, 
-              data = comb)
+# Plot the results----
+display.hmm <- function(original, 
+                        model){
+  
+  # Define colours
+  mycols <- c("darkmagenta", "turquoise")
+  
+  # Plotting the actual states for the further comparison with the HMM estimates (we look only into Norwegian states)
+  g1 <- ggplot(original, aes(x = Date, y = NO_state, fill = NO_state, col = NO_state)) + 
+    geom_bar(stat = "identity", alpha = I(0.75)) + 
+    scale_fill_manual(values = mycols, name = "State:\nProduction of\nNorway", labels = c("Deficit", "Surplus")) +
+    scale_color_manual(values = mycols, name = "State:\nProduction of\nNorway", labels = c("Deficit", "Surplus")) +
+    theme(axis.ticks = element_blank(), axis.text.y = element_blank()) +
+    labs(y = "Actual State")
+  
+  # Visualise the HMM model estimates
+  g2 <- ggplot(model, aes(x = Date, y = state, fill = state, col = state)) + 
+    geom_bar(stat = "identity", alpha = I(0.75)) +
+    scale_fill_manual(values = mycols, name = "State:\nProduction of\nNorway", labels = c("Deficit", "Surplus")) +
+    scale_color_manual(values = mycols, name = "State:\nProduction of\nNorway", labels = c("Deficit", "Surplus")) +
+    theme(axis.ticks = element_blank(), axis.text.y = element_blank()) + 
+    labs(y = "Estimated State")
+  
+  return(grid.arrange(g1, g2,
+                      widths = 1, 
+                      nrow = 2))
+}
 
-fit.hmm <- fit(hmm)
-est.states <- posterior(fit.hmm)
+# Run functions for NORD
+# Results are pretty disperse
+display.hmm(original = comb,
+            model = hmm(param = comb$NORD,
+                        data = comb))
 
-# Analyse the results
-tbl <- table(est.states$state, comb$NO_state)
-colnames(est.states)[2:3] <- c("Deficit", "Surplus")
-est.states$Date <- comb$Date
+# Run for Denmark
+# Dependece is not clear
+display.hmm(original = comb,
+            model = hmm(param = comb$DK,
+                        data = comb))
 
-grep("Deficit", comb$NO_state)
-grep("1", est.states$state)
-
-est.states$state <- gsub("1", "Deficit", est.states$state)
-est.states$state <- gsub("2", "Surplus", est.states$state)
-tbl
-
-# Plot the results
-mycols <- c("darkmagenta", "turquoise")
-
-g1 <- ggplot(comb, aes(x = Date, y = NO_state, fill = NO_state, col = NO_state)) + 
-         geom_bar(stat = "identity", alpha = I(0.75)) + 
-         scale_fill_manual(values = mycols, labels = c("Deficit", "Surplus")) +
-         scale_color_manual(values = mycols, labels = c("Deficit", "Surplus")) +
-         theme(axis.ticks = element_blank(), axis.text.y = element_blank()) +
-         labs(y = "Actual State")
-
-g2 <- ggplot(est.states, aes(x = Date, y = state, fill = state, col = state)) + 
-  geom_bar(stat = "identity", alpha = I(0.75)) +
-  scale_fill_manual(values = mycols, name = "State:\nProduction of\nNorway", labels = c("Deficit", "Surplus")) +
-  scale_color_manual(values = mycols, name = "State:\nProduction of\nNorway", labels = c("Deficit", "Surplus")) +
-  theme(axis.ticks = element_blank(), axis.text.y = element_blank()) + 
-  labs(y = "Estimated State")
-
-grid.arrange(g1, g2, 
-             widths = 1, 
-             nrow = 2)
+# Run for Sweden
+# Out of all models Swedish production~consumption parameters are most descriptive for the Norwegian states
+display.hmm(original = comb,
+            model = hmm(param = comb$SE,
+                        data = comb))
